4ca9bbc1490abaf72cede78fa4ac0f94df581916
engrbugs <engrbugs@gmail.com>
2023-05-10 22:28:05 -0700

    2023-05-10-2227

---
+++ PDF.cs
@@ -0,0 +1,137 @@
+﻿//using Microsoft.Office.Interop.Word;
+//using QuestPDF.Fluent;
+//using QuestPDF.Helpers;
+//using QuestPDF.Infrastructure;
+
+using System;
+using System.IO;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+
+using iText.Kernel.Colors;
+using iText.Kernel.Font;
+using iText.Kernel.Geom;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+using iText.IO.Font.Constants;
+using static iText.IO.Util.IntHashtable;
+using Windows.System;
+using iText.IO.Font;
+
+//using Word = Microsoft.Office.Interop.Word;
+
+namespace PDFIO;
+
+internal class createPDF
+
+{
+    public static void create_PDF(string str)
+    {
+        string head = "Dear Hiring Manager,\r\n";
+        string newLine = "\r\n";
+        string bottom = "\r\nSincerely,\r\n\r\nHerby Sio\r\n";
+        string path = "C:\\Users\\engrb\\Desktop\\hello.pdf";
+        int font_size = 11;
+        int para_botttom_spacing = 8;
+        int para_top_spacing = 0;
+
+        string str1 = "I am excited to apply for the Supply Planner position at Storkcraft. With over three years of experience in Supply and Demand Planning, I have honed my skills in data analytics, attention to detail, and collaboration to ensure an optimal balance between customer service and inventory levels. I believe my experience and skills are a perfect match for this role.";
+        string str2 = "As a former Business Analyst, I have a strong background in analyzing business intelligence data to identify areas for improvement and developing and executing solutions to drive growth and profitability. Moreover, my proficiency in Excel and other data visualization tools like Power Bl and Tableau has helped me create visually appealing reports to help stakeholders understand trends and make informed decisions.";
+        string str3 = "In my current role as a CAD & Revit Technician, I have developed and implemented efficient workflows to reduce project completion time and automate repetitive tasks, resulting in a 20% increase in productivity. My ownership mindset and strong commitment to delivering outstanding results will enable me to work diligently to ensure that Storkcraft remains the manufacturer of choice for new and existing parents.";
+        string str4 = "I am excited to contribute my skills and experience to the Storkcraft team and would be grateful for the opportunity to discuss my qualifications further. Thank you for your time and consideration.";
+
+        PdfWriter writer = new PdfWriter(path);
+        PdfDocument pdf = new PdfDocument(writer);
+        Document document = new Document(pdf);
+
+        FontProgram fontProgram =
+        FontProgramFactory.CreateFont(@"C:\Windows\Fonts\calibri.ttf");
+        PdfFont calibri = PdfFontFactory.CreateFont(fontProgram, PdfEncodings.WINANSI);
+
+        document.SetMargins(72, 72, 72, 72);
+        document.SetFont(calibri);
+        document.SetFontSize(11);
+        document.SetTextAlignment(TextAlignment.JUSTIFIED);
+
+
+
+        Paragraph para = new Paragraph(head + newLine + str1 + newLine)
+            .SetMarginBottom(8)
+            .SetMarginTop(0);
+
+        document.Add(para);
+
+        Paragraph para2 = new Paragraph(str2 + newLine)
+            .SetMarginBottom(8)
+            .SetMarginTop(0);
+
+        document.Add(para2);
+
+        Paragraph para3 = new Paragraph(str3 + newLine)
+            .SetMarginBottom(8)
+            .SetMarginTop(0);
+
+        document.Add(para3);
+
+        Paragraph para4 = new Paragraph(str4 + newLine + bottom)
+            .SetMarginBottom(8)
+            .SetMarginTop(0);
+
+        document.Add(para4);
+
+
+
+        document.Close();
+    }
+}
+
+
+    //public static void create_PDF(string str)
+    //{
+    //    str = "Dear Hiring Manager,\r\n\r\nI am excited to apply for the Supply Planner position at Storkcraft. With over three years of experience in Supply and Demand Planning, I have honed my skills in data analytics, attention to detail, and collaboration to ensure an optimal balance between customer service and inventory levels. I believe my experience and skills are a perfect match for this role.\r\nAs a former Business Analyst, I have a strong background in analyzing business intelligence data to identify areas for improvement and developing and executing solutions to drive growth and profitability. Moreover, my proficiency in Excel and other data visualization tools like Power Bl and Tableau has helped me create visually appealing reports to help stakeholders understand trends and make informed decisions.\r\nIn my current role as a CAD & Revit Technician, I have developed and implemented efficient workflows to reduce project completion time and automate repetitive tasks, resulting in a 20% increase in productivity. My ownership mindset and strong commitment to delivering outstanding results will enable me to work diligently to ensure that Storkcraft remains the manufacturer of choice for new and existing parents.\r\nI am excited to contribute my skills and experience to the Storkcraft team and would be grateful for the opportunity to discuss my qualifications further. Thank you for your time and consideration.\r\n\r\nSincerely,\r\n\r\nHerby Sio\r\n";
+    //    QuestPDF.Settings.License = LicenseType.Community;
+    //    Document.Create(container =>
+    //    {
+    //        container.Page(page =>
+    //        {
+    //            page.Size(PageSizes.Letter);
+    //            page.Margin(1, Unit.Inch);
+    //            page.PageColor(Colors.White);
+    //            page.DefaultTextStyle(x => 
+    //               x.FontSize(11)
+    //                .FontFamily(Fonts.Calibri)
+
+    //            );
+
+    //            //page.Header()
+    //            //    .Text("Hello PDF!")
+    //            //    .SemiBold().FontSize(36).FontColor(Colors.Blue.Medium);
+
+    //            page.Content()
+    //                .PaddingVertical(1, Unit.Centimetre)
+
+    //                .Column(x =>
+    //                {
+    //                    x.Spacing(20);
+    //                    x.Item().Text(text =>
+    //                    {
+    //                        text.Span(str);
+    //                        text.ParagraphSpacing(8);
+    //                    });
+
+    //                });
+
+    //            page.Footer()
+    //                .AlignCenter()
+    //                .Text(x =>
+    //                {
+    //                    //x.Span("Page ");
+    //                    //x.CurrentPageNumber();
+    //                });
+    //        });
+    //    })
+    //    .GeneratePdf("C:\\Users\\engrb\\Desktop\\hello.pdf");
+    //}
+
--- Program.cs
+++ Program.cs
@@ -1,14 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using clipboardIO;
+﻿using clipboardIO;
 using ClipboardUpdate;
 using iniFileIO;
-
+using PDFIO;
+using System.Runtime.InteropServices;
 
 namespace ChatClipV2;
 
+class Reader
+{
+    private static Thread inputThread;
+    private static AutoResetEvent getInput, gotInput;
+    private static string input;
+    private static bool stopFlag = false;
+
+
+    static Reader()
+    {
+        getInput = new AutoResetEvent(false);
+        gotInput = new AutoResetEvent(false);
+        inputThread = new Thread(reader);
+        inputThread.IsBackground = true;
+        inputThread.Start();
+    }
+
+    private static void reader()
+    {
+        while (true)
+        {
+            getInput.WaitOne();
+            input = Console.ReadLine();
+            gotInput.Set();
+        }
+    }
+
+    // omit the parameter to read a line without a timeout
+    public static string ReadLine()
+    {
+        bool success = false;
+        getInput.Set();
+        while (!stopFlag && !success)
+        {
+             success = gotInput.WaitOne(1000);
+        }
+        if (success)
+            return input;
+        else
+            throw new TimeoutException("User did not provide input within the timelimit.");
+    }
+    public static void StopReading()
+    {
+        stopFlag = true;
+        getInput.Set();
+    }
+}
+
 class Program
 {
+    
+
     static bool exit_called = false;
     static int clipBoards_index = 0;
 
@@ -47,15 +96,20 @@ class Program
         }
         clipboard.SetClipboardText(prompt);
         Console.WriteLine($"Prompt saved in clipboard.");
-        Console.Write(CONST.CONSOLE_MAIN_NAME);
+
+        Reader.StopReading();
+
     }
 
     private static void checkClipboardUpdateThread(Object state)
     {
+
+
         while (clipBoards_index != clipBoards.Count)
         {
             if (exit_called)
             {
+                
                 // Stop everything and return from the function
                 return;
             }
@@ -70,17 +124,25 @@ class Program
         }
         if (exit_called != true)
         {
+            exit_called = true;
             create_a_Prompt();
         }
+
     }
 
     static void revert()
     {
         clipBoards_index--;
     }
+    private static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+    {
+        //e.Cancel = true; // cancel the Ctrl+C key press event
+        exit_called = true; // set the input string to "x" to exit the loop that reads from the console
+    }
 
     static void Main(string[] args)
     {
+        createPDF.create_PDF("hello");
         Console.WriteLine("List of commands: " + CONST.VER);
         for (int i = 1; i <= 9; ++i)
         {
@@ -121,8 +183,8 @@ class Program
         Thread t = new Thread(checkClipboardUpdateThread);
         t.IsBackground = true;
         t.Start();
-
-        while (input_str != "x" && input_str != "exit")
+        
+        while (input_str != "x" && input_str != "exit" && exit_called != true)
         {
             if (input_str == "revert")
             {
@@ -130,8 +192,20 @@ class Program
                 Console.WriteLine($"Index reduced ({clipBoards_index}/{clipBoards.Count}).");
             }
 
-            Console.Write(CONST.CONSOLE_MAIN_NAME);
-            input_str = Console.ReadLine().Trim().ToLower();
+            try
+            {
+                Console.Write(CONST.CONSOLE_MAIN_NAME);
+                input_str = Reader.ReadLine().Trim().ToLower();
+            }
+            catch (TimeoutException e)
+            {
+                // Handle the timeout exception here...
+            }
+            catch (Exception e)
+            {
+                // Handle any other exceptions here...
+            }
+
         }
 
         // Stop the clipboard monitoring thread
@@ -140,8 +214,19 @@ class Program
         exit_called = true;
         t.Join(1000);
 
+
+        // ask if want to create a PDF
+        Console.Write("Do you want to create and overwrite PDF on path, before exit? [Y yes* or N X no]: ");
+        string create_pdf = Console.ReadLine().Trim().ToLower();
+
+        if (create_pdf == "" || create_pdf == "y" || create_pdf == "yes")
+        {
+            createPDF.create_PDF("hello");
+        }
+
         return;
     }
+    
 }
 
 
