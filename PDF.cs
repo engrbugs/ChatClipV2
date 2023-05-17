033710b5fa9ac04a15104226b5bb4a544d489b2e
engrbugs <engrbugs@gmail.com>
2023-05-16 23:29:35 -0700

    2023-05-16-2329

--- PDF.cs
+++ PDF.cs
@@ -32,7 +32,7 @@ internal class createPDF
         string head = "Dear Hiring Manager,\r\n";
         string newLine = "\r\n";
         string bottom = "\r\nSincerely,\r\n\r\nHerby Sio\r\n";
-        string path = "C:\\Users\\engrb\\Desktop\\hello.pdf";
+        string path = "C:\\Users\\engrb\\Desktop\\Herby_Sio-Cover.pdf";
         int font_size = 11;
         int para_botttom_spacing = 8;
         int para_top_spacing = 0;
@@ -44,8 +44,6 @@ internal class createPDF
         FontProgram fontProgram =
         FontProgramFactory.CreateFont(@"C:\Windows\Fonts\calibri.ttf");
         PdfFont calibri = PdfFontFactory.CreateFont(fontProgram, PdfEncodings.WINANSI);
-
-
 
 
         document.SetMargins(72, 72, 72, 72);
@@ -83,63 +81,5 @@ internal class createPDF
 
         document.Close();
 
-
-
-
-
-
-
-
-
-
     }
 }
-
-
-    //public static void create_PDF(string str)
-    //{
-    //    str = "Dear Hiring Manager,\r\n\r\nI am excited to apply for the Supply Planner position at Storkcraft. With over three years of experience in Supply and Demand Planning, I have honed my skills in data analytics, attention to detail, and collaboration to ensure an optimal balance between customer service and inventory levels. I believe my experience and skills are a perfect match for this role.\r\nAs a former Business Analyst, I have a strong background in analyzing business intelligence data to identify areas for improvement and developing and executing solutions to drive growth and profitability. Moreover, my proficiency in Excel and other data visualization tools like Power Bl and Tableau has helped me create visually appealing reports to help stakeholders understand trends and make informed decisions.\r\nIn my current role as a CAD & Revit Technician, I have developed and implemented efficient workflows to reduce project completion time and automate repetitive tasks, resulting in a 20% increase in productivity. My ownership mindset and strong commitment to delivering outstanding results will enable me to work diligently to ensure that Storkcraft remains the manufacturer of choice for new and existing parents.\r\nI am excited to contribute my skills and experience to the Storkcraft team and would be grateful for the opportunity to discuss my qualifications further. Thank you for your time and consideration.\r\n\r\nSincerely,\r\n\r\nHerby Sio\r\n";
-    //    QuestPDF.Settings.License = LicenseType.Community;
-    //    Document.Create(container =>
-    //    {
-    //        container.Page(page =>
-    //        {
-    //            page.Size(PageSizes.Letter);
-    //            page.Margin(1, Unit.Inch);
-    //            page.PageColor(Colors.White);
-    //            page.DefaultTextStyle(x => 
-    //               x.FontSize(11)
-    //                .FontFamily(Fonts.Calibri)
-
-    //            );
-
-    //            //page.Header()
-    //            //    .Text("Hello PDF!")
-    //            //    .SemiBold().FontSize(36).FontColor(Colors.Blue.Medium);
-
-    //            page.Content()
-    //                .PaddingVertical(1, Unit.Centimetre)
-
-    //                .Column(x =>
-    //                {
-    //                    x.Spacing(20);
-    //                    x.Item().Text(text =>
-    //                    {
-    //                        text.Span(str);
-    //                        text.ParagraphSpacing(8);
-    //                    });
-
-    //                });
-
-    //            page.Footer()
-    //                .AlignCenter()
-    //                .Text(x =>
-    //                {
-    //                    //x.Span("Page ");
-    //                    //x.CurrentPageNumber();
-    //                });
-    //        });
-    //    })
-    //    .GeneratePdf("C:\\Users\\engrb\\Desktop\\hello.pdf");
-    //}
-
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using ClipboardUpdate;
 using iniFileIO;
 using PDFIO;
+using System.Text;
 using System.Text.RegularExpressions;
 
 
@@ -34,26 +35,31 @@ class Program
             return str.Trim();
     }
 
-    static void create_a_Prompt()
+    static void CreateAPrompt()
     {
-        string prompt = "";
+        StringBuilder promptBuilder = new StringBuilder();
         clipBoards_index = 0;
-        for (int i = 0; i <= prompt_Lines.Count - 1; i++)
+
+        for (int i = 0; i < prompt_Lines.Count; i++)
         {
             bool exists = before_Locs.Contains(i);
+
             if (exists)
             {
-                prompt += clipBoards[clipBoards_index];
+                promptBuilder.Append(clipBoards[clipBoards_index]);
                 clipBoards_index++;
             }
-            prompt += prompt_Lines[i];
-        }
-        clipboard.SetClipboardText(prompt);
-        Console.WriteLine($"Prompt saved in clipboard.");
 
+            promptBuilder.Append(prompt_Lines[i]);
+        }
+
+        string prompt = promptBuilder.ToString();
+        clipboard.SetClipboardText(prompt);
+        Console.WriteLine("Prompt saved in clipboard.");
     }
 
-    private static void checkClipboardUpdateThread(Object state)
+
+    private static void CheckClipboardUpdate(Object state)
     {
         while (clipBoards_index != clipBoards.Count)
         {
@@ -75,19 +81,19 @@ class Program
         if (exit_called != true)
         {
             exit_called = true;
-            create_a_Prompt();
-            ask_for_cover();
+            CreateAPrompt();
+            AskForCover();
         }
 
     }
 
-    static void revert()
+    static void Revert()
     {
         clipBoards_index--;
     }
 
     static bool ask_cover_phase = false;
-    static void ask_for_cover()
+    static void AskForCover()
     {
         ask_cover_phase = true;
         // ask if want to create a PDF
@@ -98,133 +104,164 @@ class Program
 
 
 
-    static string[] separated_para(string str)
+    static string[] SeparateParagraphs(string str)
     {
-        int startIndex = 0;
+        int startIndex = FindStartIndex(str);
+        int endIndex = FindEndIndex(str);
+        string trimmedText = GetTrimmedText(str, startIndex, endIndex);
+        string[] paragraphs = SplitIntoParagraphs(trimmedText);
+        string[] nonEmptyParagraphs = RemoveEmptyParagraphs(paragraphs);
+        return nonEmptyParagraphs;
+    }
+
+    static int FindStartIndex(string str)
+    {
         foreach (string salutation in CONST.COVER_SALUTATION)
         {
-            Regex regex = new Regex(salutation);
+            Regex regex = new Regex(salutation, RegexOptions.IgnoreCase); // Add RegexOptions.IgnoreCase to make the regex case-insensitive);
             Match match = regex.Match(str);
 
             if (match.Success)
             {
-                startIndex = match.Index;
-                startIndex = str.IndexOf("\n", startIndex);
-                break; // Break the loop when the first salutation is found
+                int startIndex = match.Index;
+                return str.IndexOf("\n", startIndex);
             }
         }
 
+        return 0; // Default start index if no salutation found
+    }
+
+    static int FindEndIndex(string str)
+    {
         int endIndex = str.Length;
+
         foreach (string closing in CONST.COVER_CLOSING)
         {
-            Regex regex = new Regex(closing);
+            Regex regex = new Regex(closing, RegexOptions.IgnoreCase); // Add RegexOptions.IgnoreCase to make the regex case-insensitive
             Match match = regex.Match(str);
 
             if (match.Success)
             {
-                endIndex = match.Index + match.Length;
+                endIndex = match.Index;
                 break; // Break the loop when the first closing is found
             }
         }
 
-
-        string trimmedText = str.Substring(startIndex, endIndex - startIndex).Trim();
-        // Console.WriteLine(trimmedText.Trim());
-
-        // Console.WriteLine("#########################################");
-
-        string[] para = trimmedText.Split(new[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
-        List<string> paraList = new List<string>(para);
-
-        paraList.RemoveAll(s => string.IsNullOrEmpty(s.Trim()));
-
-        para = paraList.ToArray();
-        //foreach (string paragraph in para)
-        //{
-        //    Console.WriteLine(paragraph);
-        //}
-        return para;
+        return endIndex;
     }
+
+    static string GetTrimmedText(string str, int startIndex, int endIndex)
+    {
+        string trimmedText = str.Substring(startIndex, endIndex - startIndex).Trim();
+        return trimmedText;
+    }
+
+    static string[] SplitIntoParagraphs(string str)
+    {
+        string[] paragraphs = str.Split(new[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
+        return paragraphs;
+    }
+
+    static string[] RemoveEmptyParagraphs(string[] paragraphs)
+    {
+        List<string> nonEmptyParagraphs = new List<string>(paragraphs);
+        nonEmptyParagraphs.RemoveAll(s => string.IsNullOrEmpty(s.Trim()));
+        return nonEmptyParagraphs.ToArray();
+    }
+
 
     static void Main(string[] args)
     {
         Console.WriteLine("List of commands: " + CONST.VER);
-        for (int i = 1; i <= 9; ++i)
+
+        ReadFiles();
+
+        DisplayCommands(CONST.COMMANDS);
+        DisplayCommands(CONST.EXIT_COMMANDS);
+
+        Thread clipboardThread = new Thread(CheckClipboardUpdate);
+        clipboardThread.IsBackground = true;
+        clipboardThread.Start();
+        string input;
+        while (true)
         {
-            string str = "";
+            input = GetUserInput();
 
-            // read for [Files] category 
-            str = iniFIle.Read(ExePath() + CONST.FILES_TO_READ, "Files", i.ToString());
-            if (!string.IsNullOrEmpty(str))
-            {
-                prompt_Lines.Add(File.ReadAllText((ExePath() + "\\" +
-                    remove_Comment_Section(str))));
-            }
-
-            // read for [Before] category 
-            str = iniFIle.Read(ExePath() + CONST.FILES_TO_READ, "Before", i.ToString());
-            if (!string.IsNullOrEmpty(str))
-            {
-                //  minus 1 because i'm using zero-indexed but my ini file is in one-based
-                before_Locs.Add(int.Parse(remove_Comment_Section(str)) - 1);
-                clipBoards.Add("");
-            }
-        }
-
-        List<string> triggers = new List<string>();
-        foreach (KeyValuePair<string, string[]> x in CONST.COMMANDS)
-        {
-            Console.WriteLine(x.Key + ": [" + x.Value[1] + "]" + x.Value[0]);
-            triggers.Add(x.Value[0]);
-            triggers.Add(x.Value[1]);
-        }
-        foreach (KeyValuePair<string, string[]> x in CONST.EXIT_COMMANDS)
-        {
-            Console.WriteLine(x.Key + ": [" + x.Value[1] + "]" + x.Value[0]);
-        }
-
-        string? input_str = "";
-
-        Thread t = new Thread(checkClipboardUpdateThread);
-        t.IsBackground = true;
-        t.Start();
-
-
-        while (input_str != "x" && input_str != "exit")
-        {
             if (ask_cover_phase)
             {
                 break;
             }
-            else if (input_str == "revert")
+            else if (input == "revert")
             {
-                revert();
+                Revert();
                 Console.WriteLine($"Index reduced ({clipBoards_index}/{clipBoards.Count}).");
             }
-
-            Console.Write(CONST.CONSOLE_MAIN_NAME);
-            input_str = Console.ReadLine().Trim().ToLower();
-
+            else if (IsExitCommand(input))
+            {
+                break;
+            }
         }
 
         string cover;
-        if (input_str.ToLower().Trim() == "" ||
-        input_str.ToLower().Trim() == "y" ||
-        input_str.ToLower().Trim() == "yes")
+        input = input.ToLower().Trim();
+
+        if (ShouldGeneratePDF(input))
         {
-            Console.Write("Will read on clipboard for the cover context [Press ENTER]");
+            Console.Write("Will read the cover context from the clipboard [Press ENTER]");
             Console.ReadLine();
-            //createPDF.create_PDF("hello");
             cover = clipboard.GetClipboardText();
-            string[] paras = separated_para(cover);
-            createPDF.create_PDF(paras);
+            string[] paragraphs = SeparateParagraphs(cover);
+            createPDF.create_PDF(paragraphs);
         }
 
-        t.Join(1000);
-
-        return;
+        clipboardThread.Join(1000);
     }
 
+    static void ReadFiles()
+    {
+        for (int i = 1; i <= 9; ++i)
+        {
+            string file = iniFIle.Read(ExePath() + CONST.FILES_TO_READ, "Files", i.ToString());
+            if (!string.IsNullOrEmpty(file))
+            {
+                string promptLine = File.ReadAllText((ExePath() + "\\" + remove_Comment_Section(file)));
+                prompt_Lines.Add(promptLine);
+            }
+
+            string before = iniFIle.Read(ExePath() + CONST.FILES_TO_READ, "Before", i.ToString());
+            if (!string.IsNullOrEmpty(before))
+            {
+                int beforeLoc = int.Parse(remove_Comment_Section(before)) - 1;
+                before_Locs.Add(beforeLoc);
+                clipBoards.Add("");
+            }
+        }
+    }
+
+    static void DisplayCommands(Dictionary<string, string[]> commands)
+    {
+        foreach (KeyValuePair<string, string[]> command in commands)
+        {
+            Console.WriteLine(command.Key + ": [" + command.Value[1] + "]" + command.Value[0]);
+        }
+    }
+
+    static string GetUserInput()
+    {
+        Console.Write(CONST.CONSOLE_MAIN_NAME);
+        return Console.ReadLine().Trim().ToLower();
+    }
+
+
+    static bool IsExitCommand(string input)
+    {
+        return input == "x" || input == "exit";
+    }
+
+    static bool ShouldGeneratePDF(string input)
+    {
+        return string.IsNullOrEmpty(input) || input == "y" || input == "yes";
+    }
 }
 
 
